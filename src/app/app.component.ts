e5f54d2e875d2341fdce9936d8a47e2a8833c0ab
Joel Gallardo Manjarrez <joelgallardomn@gmail.com>
2023-10-21 00:03:26 -0700

    new comp & Sharing data between child and parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  //arreglo de objetos
-  peliculas;
-
   // es un metodo del ciclo de vida de un componente
   // lo que hace esque permite ejecutar una funcionalidad antes que se inicialice el componente
   ngOnInit(): void {
 
     setTimeout(() => {
-      this.peliculas = [];
-    }, 500);
+      this.peliculasEnCines =
+        [
+          {
+            titulo: 'Spiderman',
+            fechaLanzamiento: new Date(),
+            precio: 123.04
+          },
+          {
+            titulo: 'Pulp fiction',
+            fechaLanzamiento: new Date(),
+            precio: 444.05
+          }
+        ];
+    }, 5000);
 
   }
+
+  //arreglo de objetos
+  peliculasEnCines;
+
+  peliculasProximosEstrenos =
+    [
+      {
+        titulo: 'La toalla del mojado',
+        fechaLanzamiento: new Date(),
+        precio: 1523.04
+      },
+      {
+        titulo: 'Sherk 5',
+        fechaLanzamiento: new Date(),
+        precio: 4644.05
+      },
+      {
+        titulo: 'Rapido y furioso 20',
+        fechaLanzamiento: new Date(),
+        precio: 5644.05
+      }
+    ];
 
   //variable
   title = 'hello world!';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListadoPeliculasComponent } from './peliculas/listado-peliculas/listado-peliculas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoPeliculasComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
 })
 export class AppModule { }
